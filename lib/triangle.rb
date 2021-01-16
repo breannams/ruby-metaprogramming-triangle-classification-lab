1097a1b0d6f309e647e91f8b5965a259dc6c266e
breannams <breannamarie_s@hotmail.com>
2021-01-15 19:07:52 -0500

    commit tree 81d4e31535ff151fa8a0f5c04eb62f285de7eeb9

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+ def initialize(l1, l2, l3)
+  @l1,@l2, @l3 = [l1,l2,l3].sort
+
+  end
+
+  def kind
+    raise TriangleError if @l1 + @l2 <= @l3
+
+
+   if
+       @l1 == @l2 && @l2 == @l3
+       :equilateral
+      elsif @l1 == @l2 || @l1 == @l3 || @l2 == @l3
+        :isosceles
+     
+      else 
+        :scalene
+      end
+    end
+
+
+class TriangleError < StandardError
+end
+
 end
